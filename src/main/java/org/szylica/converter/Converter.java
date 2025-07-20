ce7d7084ade27e51629eebf3a76a2c15eb9abdf6
szylica <szymonsta12@onet.pl>
2025-07-20 13:02:40 +0200

    converter wireframe, from carData object to Car object

---
+++ src/main/java/org/szylica/converter/Converter.java
@@ -0,0 +1,4 @@
+package org.szylica.converter;
+
+public interface Converter {
+}
---
+++ src/main/java/org/szylica/converter/car/CarsDataToCarsConverter.java
@@ -0,0 +1,10 @@
+package org.szylica.converter;
+
+import org.szylica.data.model.CarsData;
+import org.szylica.model.car.Car;
+
+import java.util.List;
+
+public interface CarsDataToCarsConverter extends Converter<CarsData, List<Car>> {
+
+}
